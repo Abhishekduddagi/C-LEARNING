58b1e661111479d006da2c57fbd11c3e43df5518
Abhishekduddagi <164056512+Abhishekduddagi@users.noreply.github.com>
2024-03-25 19:15:37 +0530

    Update STONEPAPERSCISSOR.cpp

--- STONEPAPERSCISSOR.cpp
+++ STONEPAPERSCISSOR.cpp
@@ -1,63 +1,91 @@
 #include<iostream>
+#include<time.h>
 #include<string.h>
-#include<iomanip>
-#include <time.h>
 using namespace std;
- int main()
+
+int main()
 {
     system("cls");
-
+    int computer,number=3;
+    int user; 
+    char store;
      srand(time(NULL));
 
-    int computerresult=0;
-    int number=3;
-    int input;
-
-    cout<<"..........................................................................................."<<endl;
-    cout<<"STONE PAPER SCISSOR GAME"<<endl;
-    cout<<"..........................................................................................."<<endl;
-    cout<<endl;
+    cout<<"THIS IS A GAME OF STONE PAPER SCISSOR"<<endl;
+    cout<<".............................................................................................";
     cout<<"MENU"<<endl;
     cout<<"ENTER 1 FOR PAPER"<<endl;
     cout<<"ENTER 2 FOR STONE"<<endl;
-    cout<<"ENTER 3 FOR SCISOOR"<<endl;    
-    cout<<endl;
-    cin>>input;
-    switch (input)
+    cout<<"ENTER 3 FOR SCISSOR"<<endl;   
+    cout<<"............................................................................................."<<endl;
+    cin>>user;
+    switch (user)
     {
     case 1:
-        cout<<"paper"<<endl;
+    cout<<"PAPER";
         break;
     case 2:
-        cout<<"STONE"<<endl;
+    cout<<"STONE";
         break;
     case 3:
-        cout<<"SCISSOR"<<endl;
+    cout<<"SCISSOR";
         break;
     default:
-        cout<<"INVALID"<<endl;
+    cout<<"INVALID ENTRY";
         break;
     }
-    computerresult=(rand()%number)+1;
-   
-    cout<<"\n AI BOT CHOOSES :"<<computerresult<<"\t"<<endl;
-    switch (computerresult)
+    computer=(rand()%number)+1;
+    cout<<"\n AI BOT CHOOSES :"<<computer<<"\t"<<endl;
+   if (user==1)
+   { 
+    if ( computer==1 )
     {
-    case 1:
-        cout<<"PAPER"<<endl;
-        break;
-    case 2:
-        cout<<"STONE"<<endl;
-        break;    
-    case 3:
-        cout<<"SCISSOR"<<endl;
-        break;
-    default:
-    cout<<"INVALID"<<endl;
-        break;
+    cout<< "THIS IS TIE"<<endl;
+    }
+    else if (computer== 2 )
+    {
+    cout<< "YOU WIN"<<endl;
     }
     
-     system("PAUSE");
-     
-return 0;     
+    else
+    {
+    cout<<"YOU LOSS"<<endl;
+    }
+    }
+   
+    else if (user==2)
+   { 
+    if ( computer==1 )
+    {
+    cout<< "YOU LOSS"<<endl;
+    }
+    else if (computer== 2 )
+    {
+    cout<< "THIS IS TIE"<<endl;
+    }
+    
+    else
+    {
+    cout<<"YOU WIN"<<endl;
+    }
+    }
+
+    else
+   { 
+    if ( computer==1 )
+    {
+    cout<< "YOU WIN"<<endl;
+    }
+    else if (computer== 2 )
+    {
+    cout<< "YOU LOSS"<<endl;
+    }
+    
+    else
+    {
+    cout<<"THIS IS A TIE"<<endl;
+    }
+    }
+
+return 0;
 }
